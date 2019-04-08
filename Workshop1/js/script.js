58b91814c0066d9a19877fa100821650f645e1f8
LinMark0320 <f3842250@gmail.com>
2019-04-09 03:49:31 +0800

    0409

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -5,7 +5,8 @@ var bookCategoryList = [
     { text: "網際網路", value: "internet", src: "image/internet.jpg" },
     { text: "應用系統整合", value: "system", src: "image/system.jpg" },
     { text: "家庭保健", value: "home", src: "image/home.jpg" },
-    { text: "語言", value: "language", src: "image/language.jpg" }
+    { text: "語言", value: "language", src: "image/language.jpg" },
+    { text: "   ", value:"1990-08-14" }
 ];
 
 // 載入書籍資料
